1c3e9c4dcaf2eab1aae4d34a617d767e36ddf344
ProgrammingHero <tamimhossain6421@gmail.com>
2023-10-20 12:55:44 +0600

    added verce.json code and deploy

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ async function run() {
     // added add to cart data to the database 
     app.post('/carts', async (req, res) =>{
       const users = req.body 
-      console.log(users)
+      // console(users)
+      delete users._id
+      // console.log(users)
       const result = await addToCart.insertOne(users)
       res.send(result)
     })
@@ -100,7 +102,15 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id 
+      console.log('delete id', id)
+      const query = {_id: new ObjectId(id)}
+      const result = await addToCart.deleteOne(query)
+      res.send(result)
+    })
+
+ 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
