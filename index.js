e1457901d122d6456f1a20b4af6155501903952b
ProgrammingHero <tamimhossain6421@gmail.com>
2023-10-20 12:57:42 +0600

    added verce.json code and deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 require('dotenv').config()
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port= process.env.PORT || 5000
 
 
 // here I used to middleware 
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const productCollection = client.db("productDB").collection("products")
     const addToCart = client.db("productDB").collection("cartCollection")
@@ -113,7 +113,7 @@ async function run() {
  
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+ 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
