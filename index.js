3ca25a011754b10ab6ba83b323c2225a7109f793
ProgrammingHero <tamimhossain6421@gmail.com>
2023-12-14 19:54:09 +0600

    make post route for sslcommerz

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const SSLCommerzPayment = require('sslcommerz-lts')
 const port= process.env.PORT || 5000
+
 
 
 // here I used to middleware 
@@ -26,6 +28,11 @@ const client = new MongoClient(uri, {
   }
 });
 
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false //true for live, false for sandbox
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +40,94 @@ async function run() {
 
     const productCollection = client.db("productDB").collection("products")
     const addToCart = client.db("productDB").collection("cartCollection")
+    const userCollection = client.db("productDB").collection("userCollection")
+    const orderCollection = client.db("productDB").collection("orderCollection")
+    
+
+    // post payment information 
+app.post('/order', async(req, res) =>{
+  const product = await addToCart.findOne({_id: new ObjectId(req.body.productId)})
+  // console.log(product)
+
+  const tran_id = new ObjectId().toString()
+  const order = req.body
+  const data = {
+    total_amount: parseFloat(product.price),
+    currency: 'BDT',
+    tran_id: tran_id, // use unique tran_id for each api call
+    success_url: `http://localhost:5000/payment/success/${tran_id}`,
+    fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+    cancel_url: 'http://localhost:3030/cancel',
+    ipn_url: 'http://localhost:3030/ipn',
+    shipping_method: 'Courier',
+    product_name: 'Computer.',
+    product_category: 'Electronic',
+    product_profile: 'general',
+    cus_name: order.name,
+    cus_email: order.userEmail,
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: '01711111111',
+    cus_fax: '01711111111',
+    ship_name: 'Customer Name',
+    ship_add1: 'Dhaka',
+    ship_add2: 'Dhaka',
+    ship_city: 'Dhaka',
+    ship_state: 'Dhaka',
+    ship_postcode: 1000,
+    ship_country: 'Bangladesh',
+};
+
+console.log(data)
+
+const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+sslcz.init(data).then(apiResponse => {
+    // Redirect the user to payment gateway
+    let GatewayPageURL = apiResponse.GatewayPageURL;
+    res.send({url:GatewayPageURL})
+
+    const finalOrder = {
+      product,
+      paidStatus: false,
+      tranjectionId: tran_id,
+
+    }
+    const result =  orderCollection.insertOne(finalOrder)
+    console.log('Redirecting to: ', GatewayPageURL)
+});
+
+// payment success Post 
+app.post("/payment/success/:tranId", async(req, res) => {
+  console.log(req.params.tranId)
+  const result = await orderCollection.updateOne({ tranjectionId:req.params.tranId },{
+    $set:{
+      paidStatus: true,
+    }
+  })
+  if(result.modifiedCount > 0){
+    res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`)
+  }
+ 
+})
+
+
+// payment failed post 
+app.post('/payment/fail/:tranId', async( req, res) =>{
+  const result = await orderCollection.deleteOne({ tranjectionId:req.params.tranId })
+  if(result.deletedCount ){
+    res.redirect(`http://localhost:5173/payment/fail/${req.params.tranId}`)
+  }
+})
+
+
+})
+
+
+
  
     // post product in database
     app.post('/products', async(req, res) =>{
@@ -110,7 +205,27 @@ async function run() {
       res.send(result)
     })
 
- 
+
+    
+
+
+
+    // users information post 
+    // app.post('/users', async(req, res) =>{
+    //   const newUser = req.body
+    //   console.log(newUser)
+    //   const result = await userCollection.insertOne(newUser)
+    //   res.send(result)
+    // })
+
+ // read the data of users
+    // app.get('/users', async(req, res) =>{
+    //   const cursor = userCollection.find()
+    //   const result = await cursor.toArray()
+    //   res.send(result)
+    // })
+
+
 
     // Send a ping to confirm a successful connection
  
